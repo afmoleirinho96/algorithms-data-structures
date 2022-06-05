5636e27e1aa49564b7f8152bf5ad0aeb096ab460
andremoleirinho <andre.moleirinho@verve-capital.com>
2022-06-05 19:58:38 +0100

    First commit. Add Binary Search.

---
+++ algorithms/BinarySearch.js
@@ -0,0 +1,26 @@
+const binarySearch = (nums, target) => {
+
+    let start = 0;
+    let end = nums.length;
+
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        }
+        else if (nums[mid] > target) {
+            end = mid - 1;
+        }
+
+        else {
+            start = mid + 1;
+        }
+    }
+
+    return -1;
+
+};
+
+console.log(binarySearch([-1,0,3,5,9,12], 9));
+
