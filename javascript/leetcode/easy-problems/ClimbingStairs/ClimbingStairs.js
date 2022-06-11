4109a9f891ea4905062399ee83dfc1a799e93451
andremoleirinho <andre.moleirinho@verve-capital.com>
2022-06-11 22:44:56 +0100

    Minor change in Climbing Stairs.

--- javascript/leetcode/easy-problems/ClimbingStairs/ClimbingStairs.js
+++ javascript/leetcode/easy-problems/ClimbingStairs/ClimbingStairs.js
@@ -1,7 +1,7 @@
 /* 70. https://leetcode.com/problems/climbing-stairs/ */
 
 // Time: O(n), Space: O(n)
-var climbStairs = function(n, memo = []) {
+var climbStairs = function(n, memo = {}) {
     if (n === 1) {
         return 1;
     }
@@ -38,13 +38,21 @@ const climbStairs3 = (n) => {
     return dp[n];
 };
 
+const climbStairs4 = (n) => {
 
-/*
+    if (n<=3) {
+        return n;
+    }
+
+}
+
+
+
 const t0 = performance.now();
-climbStairs(45);
+climbStairs(10000);
 const t1 = performance.now();
 console.log('Time with memoization -> ', t1 - t0); // ~= 0.03 milliseconds
-*/
+
 
 
 
@@ -57,7 +65,7 @@ console.log('Time ', t3 - t2); // ~= 4327 milliseconds - huge difference
 */
 
 const t4 = performance.now();
-climbStairs3(100);
+climbStairs3(100000);
 const t5 = performance.now();
 console.log('Time with DP ', t5 - t4); // Faster with bigger numbers and prevents 'Maximum call stack size exceeded'
 
