bae556b71a56b4f3d0c7111ee1ba6c5706935605
andremoleirinho <andre.moleirinho@verve-capital.com>
2022-06-11 22:41:59 +0100

    Solve LeetCode problem Middle of Linked List.

---
+++ javascript/leetcode/easy-problems/MiddleLinkedList/MiddleLinkedList.js
@@ -0,0 +1,16 @@
+/* 876. https://leetcode.com/problems/middle-of-the-linked-list/ */
+
+// Time: O(n), Space: O(1)
+
+var middleNode = function(head) {
+
+
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+};
